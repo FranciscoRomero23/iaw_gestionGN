6561deac13c2722114d466523f65b85d675451b6
Vagrant Default User <vagrant@servidor>
2018-11-27 11:13:59 +0000

    Cambiada base de datos en settings.py

--- gestion/settings.py
+++ gestion/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'franciscoromero2$apppython',
+        'USER': 'franciscoromero2',
+        'PASSWORD': 'usuario1234',
+        'HOST': 'franciscoromero25.mysql.pythonanywhere-services.com',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
